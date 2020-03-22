eee5c4647e62fbf8df013e57ef1082837a834c7d
Michal Dobrzycki <michal.dobrzycki@coderslab.pl>
2020-03-22 11:37:42 +0100

    Kolejne zadania

--- kolejne.py
+++ kolejne.py
@@ -1,2 +1,24 @@
-def test():
-    pass
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route("/licz/<int:a>/<int:b>")
+def dodawanie(a, b):
+    return f"{a} + {b} = {a+b}"
+
+
+@app.route("/licz")
+def dodawanie_query():
+    try:
+        a = int(request.args.get('a'))
+        b = int(request.args.get('b'))
+        return f"query wynik {a} + {b} = {a + b}"
+    except ValueError:
+        return "parametry a oraz b muszą być liczbą"
+
+
+
+if __name__ == "__main__":
+    app.run()
